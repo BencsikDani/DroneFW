a8a9e343224da5b89c3bbc039990ba16e6f47cfe
Bencsik <daniel.bencsik@harman.com>
2023-05-14 19:57:05 +0200

    Debug feature added

--- Core/Src/TaskDiagnostics.c
+++ Core/Src/TaskDiagnostics.c
@@ -1,9 +1,17 @@
 #include "main.h"
 #include "cmsis_os.h"
+#include "string.h"
 #include "Globals.h"
 
+#include "Debug.h"
+
 extern UART_HandleTypeDef huart5;
-extern osSemaphoreId BinarySemHandle;
+extern osSemaphoreId RemoteBufferSemHandle;
+extern osSemaphoreId RemoteDataSemHandle;
+extern osSemaphoreId ImuSemHandle;
+extern osSemaphoreId MagnSemHandle;
+extern osSemaphoreId DistSemHandle;
+extern osSemaphoreId GpsSemHandle;
 
 void TaskDiagnostics(void const *argument)
 {
@@ -12,16 +20,31 @@ void TaskDiagnostics(void const *argument)
 	/* Infinite loop */
 	while (1)
 	{
-		if (osSemaphoreWait(BinarySemHandle, osWaitForever) == osOK)
+		Log("Diag - RDSemEnter");
+		if(osSemaphoreWait(RemoteDataSemHandle, osWaitForever) == osOK)
 		{
-			// Setting up UART log info
+			//Log("Diag - RDSemEntered");
+			sprintf(str, "Thrust: %d\r\n", Thrust);
+
+			Log("Diag - RDSemRelease");
+			osSemaphoreRelease(RemoteDataSemHandle);
+			//Log("Diag - RDSemReleased");
+		}
+
+		Log("Diag - ISemEnter");
+		if (osSemaphoreWait(ImuSemHandle, osWaitForever) == osOK)
+		{
+			//Log("Diag - ISemEntered");
 			sprintf(str,
-					"Speed: %d\r\nTemp: %.4f\r\nAcc:  %5d ; %5d ; %5d\r\nGyro: %5d ; %5d ; %5d\r\nMagn: %5d ; %5d ; %5d\r\n\r\n",
-					speed, TempData, AccData[0], AccData[1], AccData[2],
+					"%sTemp: %.4f\r\nAcc:  %5d ; %5d ; %5d\r\nGyro: %5d ; %5d ; %5d\r\nMagn: %5d ; %5d ; %5d\r\n\r\n",
+					str,
+					TempData, AccData[0], AccData[1], AccData[2],
 					GyroData[0], GyroData[1], GyroData[2], MagData[0],
 					MagData[1], MagData[2]);
 
-			osSemaphoreRelease(BinarySemHandle);
+			Log("Diag - ISemRelease");
+			osSemaphoreRelease(ImuSemHandle);
+			//Log("Diag - ISemReleased");
 		}
 
 		// Sending UART log info
