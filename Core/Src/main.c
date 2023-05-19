8651a47d88913b5714dcfd7e595743e27fc314b4
Bencsik <daniel.bencsik@harman.com>
2023-05-19 21:39:35 +0200

    More Motor calibration refinement

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -654,6 +654,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, SPI2_IMU_CSIMU_Pin|SPI2_IMU_CSBM_Pin, GPIO_PIN_RESET);
